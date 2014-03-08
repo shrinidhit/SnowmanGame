a45b510ec021f1b65d723f25baecb5ca8d2246c8
SeongHyeok Im <ishez9210@gmail.com>
2014-03-07 20:30:10 -0500

    Initialize 'snowman.py'.

---
+++ snowman.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  7 20:18:57 2014
+
+@author:
+    SeongHyeok Im
+    Shrinidhi Thirumalai
+    Inseong Joe
+"""
+
+import pygame
+from pygame.locals import *
+import random
+import math
+import time
+
+class SnowManModel:
+    """ """
+    def __init__(self):
+        # initialize
+        pass
+    def CreateSnowman(self, x, y):
+        pass
+
+    def CreateSnowBall(self, radius, x, y):
+        pass
+
+    def RemoveSnowball(self, Snowball):
+        pass
+
+    def GetScore(self, num_snow_balls, ellapsed_time):
+        pass
+
+class SnowMan:
+    """ """
+    def __init__(self):
+        pass
+
+class SnowBall:
+    """ """
+    def __init__(self):
+        pass
+
+class SnowManView:
+    """ """
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+
+class SnowManMouseController:
+    """ """
+    def __init__(self):
+        pass
+
+    def HandleMouseEvent(self):
+        pass
+
+class SnowManSnowBallController:
+    """ """
+    def __init__(self):
+        pass
+
+class SnowManCollisionController:
+    """ """
+    def __init__(self):
+        pass
