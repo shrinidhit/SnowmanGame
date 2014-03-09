f38eae6fd1fe9a7fd8957efb9237498c0131aa05
Inseong Joe <inseong.joe@students.olin.edu>
2014-03-08 21:55:24 -0500

    working on snowmanmodel

--- snowman.py
+++ snowman.py
@@ -15,28 +15,44 @@ import math
 import time
 
 class SnowManModel:
-    """ """
+    """Encodes overall game state of Snowman game"""
     def __init__(self):
         # initialize
-        pass
-    def CreateSnowman(self, x, y):
-        pass
+        self.snowman = SnowMan(200,100,screen_height,screen_width/2.0-50,0) # set screen_width
+        self.babsoner = []
+        self.score = 0
+    #def CreateSnowman(self, x, y)
 
-    def CreateSnowBall(self, radius, x, y):
-        pass
+#change to babsoner -- radius changes to width and height
+#random -- height and width ratio stays same
+# In controller calculate time it takes babsoner to reach bottom
+# When reaches bottom call removebabsoner
+# -> count number of times removebabsoner called -> num_babsoners -> score
 
-    def RemoveSnowball(self, Snowball):
-        pass
+    def CreateBabsoner(self):
+            a = random.randint(50,150)
+            babson = Babsoner(a,a*2,random.randint(a/2.0,screen_width-a/2.0),0,)
+            self.babsoner.append(babson)
+        
+    def RemoveBabsoner(self, babsoner):
+        
 
-    def GetScore(self, num_snow_balls, ellapsed_time):
-        pass
+
+    def GetScore(self, , ellapsed_time):
+        
+        final_score = num_snow_balls + ellapsed_time
+        return final_score
+    
+    def update(self):
+        self.snowman.update()
+        self.score.update()
 
 class SnowMan:
     """ """
     def __init__(self):
         pass
 
-class SnowBall:
+class Babsoner:
     """ """
     def __init__(self):
         pass
